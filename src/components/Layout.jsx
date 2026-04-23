05d64352730caf2ed926c620f21fb7e9cacfdfe2
VanLH23 <vanlh23@fpt.com>
2026-04-23 13:41:26 +0700

    Center buttons

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,24 +1,21 @@
-import { Link, useLocation } from 'react-router-dom'
+import { Link, useLocation } from "react-router-dom";
 
 function Layout({ children }) {
-  const location = useLocation()
-  const isHome = location.pathname === '/'
+  const location = useLocation();
+  const isHome = location.pathname === "/";
 
   return (
     <div className="container">
       <header>
-        <h1>JSON Utils</h1>
         {!isHome && (
           <nav className="nav">
             <Link to="/">← Back to Home</Link>
           </nav>
         )}
       </header>
-      <main>
-        {children}
-      </main>
+      <main>{children}</main>
     </div>
-  )
+  );
 }
 
-export default Layout
+export default Layout;
